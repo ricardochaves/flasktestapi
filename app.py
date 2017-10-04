1c3cd9c3939f0415ecac7c815210280ec834b2d6
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2017-10-04 17:04:42 -0300

    criando rota home

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/homeglobomais/", methods=['POST', 'GET'])
+def homegmais():
+    if request.method == "GET":
+        obj = {'nome': 'teste', 'id': 1}
+        response = jsonify(obj)
+        response.status_code = 200
+        return response
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5001)
